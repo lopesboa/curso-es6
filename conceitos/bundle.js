d8522830b4195a461669ac1c8a734d4a19aecb1b
Lopesboa <lopesboa1@gmail.com>
2019-03-05 13:48:49 -0300

    ARRAY Operations

--- conceitos/bundle.js
+++ conceitos/bundle.js
@@ -32,13 +32,50 @@
 // }
 // console.log(Matematica.soma(1, 2));
 // ############ CONT & LET ############
-function teste(x) {
-  var y = 2;
+// function teste(x){
+//     let y = 2;
+//     if(x > 5){
+//         let y = 4;
+//         console.log(x, y);
+//     }
+// }
+// teste(10);
+// ############ ARRAY OPARATIONS ############
+var arr = [1, 3, 4, 5, 8, 9];
+/**
+*   MAP
+*   It serves to traverse the array and return a new or the same information
+*/
 
-  if (x > 5) {
-    var _y = 4;
-    console.log(x, _y);
-  }
-}
+var newArr = arr.map(function (item, index) {
+  return item + index;
+});
+console.log(newArr);
+/**
+ *  REDUCE
+ * Serves to consume every ARRAY an transform into a single 
+ * variable, usually a number
+ * */
 
-teste(10);
+var sum = arr.reduce(function (total, next) {
+  return total + next;
+});
+console.log(sum);
+/**
+ * FILTER
+ * Serves to filter a ARRAY */
+
+var filter = arr.filter(function (item) {
+  return item % 2 === 0;
+});
+console.log(filter);
+/**
+ * FILTER
+ * Is used to check if there is any information inside in the 
+ * array or if we can find this information inside the array
+ */
+
+var find = arr.find(function (item) {
+  return item === 4;
+});
+console.log(find);
--- conceitos/main.js
+++ conceitos/main.js
@@ -41,14 +41,57 @@
 
 
 // ############ CONT & LET ############
-function teste(x){
-    let y = 2;
+// function teste(x){
+//     let y = 2;
 
-    if(x > 5){
-        let y = 4;
-        console.log(x, y);
-    }
-}
+//     if(x > 5){
+//         let y = 4;
+//         console.log(x, y);
+//     }
+// }
 
-teste(10);
+// teste(10);
 
+// ############ ARRAY OPARATIONS ############
+const arr = [1, 3, 4, 5, 8, 9];
+
+/**
+*   MAP
+*   It serves to traverse the array and return a new or the same information
+*/
+const newArr = arr.map(function(item, index){
+    return item + index;
+});
+
+console.log(newArr);
+
+/**
+ *  REDUCE
+ * Serves to consume every ARRAY an transform into a single 
+ * variable, usually a number
+ * */
+const sum = arr.reduce(function(total, next){
+    return total + next;
+});
+
+console.log(sum);
+
+/**
+ * FILTER
+ * Serves to filter a ARRAY */ 
+const filter = arr.filter(function(item){
+    return item % 2 === 0;
+});
+
+console.log(filter);
+
+/**
+ * FILTER
+ * Is used to check if there is any information inside in the 
+ * array or if we can find this information inside the array
+ */
+const find = arr.find(function(item){
+    return item === 4;
+});
+
+console.log(find);
