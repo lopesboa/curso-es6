dd77ae3312bd8bb2d248cf496693aa450aae89cf
lopesboa <lopesboa1@gmail.com>
2019-03-11 14:41:33 -0300

    App com ES6

--- src/main.js
+++ src/main.js
@@ -1,15 +1,69 @@
-import axios from 'axios';
+import api from "./api";
 
-class Api{
-    static async getUserInfo(username){
-        try{
-            const response = await axios.get(`https://api.github.com/users/${username}`);
-            console.log(response);
-        }catch(err){
-            console.warn('Erro na API');
-        }
+class App{
+    constructor(){
+        this.repositories = [];
+
+        this.formEl = document.getElementById('repo-form');
+        this.inputEl = document.querySelector('input[name=repository]');
+        this.listEl = document.getElementById('repo-list');
+
+        this.registerHandler();
+    }
+
+    registerHandler(){
+        this.formEl.onsubmit = event => this.addRepository(event);
+    }
+
+    async addRepository(event){
+        event.preventDefault();
+
+        const repoInput = this.inputEl.value;
+
+        if(repoInput.length === 0)
+            return;
+
+        const response = await api.get(`/repos/${repoInput}`);
+
+        console.log(response);
+
+        this.repositories.push({
+            name: 'rocketseat.com.br',
+            description: 'Tire a sua ideia do papel e dê vida à sua startup.',
+            avatar_url: 'https://avatars0.githubusercontent.com/u/28929274?v=4',
+            html_url: 'http://github.com/rocketseat/rocketseat.com.br',
+        });
+
+        this.render();
+    }
+
+    render(){
+        this.listEl.innerHTML = '';
+
+        this.repositories.forEach(repo => {
+            let imgEl = document.createElement('img');
+            imgEl.setAttribute('src', repo.avatar_url);
+
+            let titleEl = document.createElement('strong');
+            titleEl.appendChild(document.createTextNode(repo.name));
+
+            let descriptionEl = document.createElement('p');
+            descriptionEl.appendChild(document.createTextNode(repo.description));
+
+            let linkEl = document.createElement('a');
+            linkEl.setAttribute('http', repo.html_url);
+            linkEl.setAttribute('target', '_blank');
+            linkEl.appendChild(document.createTextNode('Acessar'));
+
+            let listItemEl = document.createElement('li');
+            listItemEl.appendChild(imgEl);
+            listItemEl.appendChild(titleEl);
+            listItemEl.appendChild(descriptionEl);
+            listItemEl.appendChild(linkEl);
+
+            this.listEl.appendChild(listItemEl);
+        });
     }
 }
 
-Api.getUserInfo('lopesboa');
-Api.getUserInfo('lopesboa346346');
+new App();
